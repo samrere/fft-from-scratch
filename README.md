2db34c388f2242b3acfae28e29d4501949335e10
yu hou <yhou0015@student.monash.edu>
2021-12-14 21:59:16 +1100

    a

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This is a Python implementation of Fast Fourier Transform (FFT) in 1d and 2d fro
 The honeycomb pattern on old photos is due to the "silk finish" paper texture, which was used a lot back in the day. We can remove them in frequency domain using fft.
 #### view on larger screen, small screen will show aliasing effect. We can use a low pass filter later.
 <p align="center">
-  <img src="https://github.com/samrere/fft-from-scratch/blob/main/images/animation.gif" width="1000">
+  <img src="https://github.com/samrere/fft-from-scratch/blob/main/images/slider.gif" width="1000">
 </p>
 
 ### [*Convolution*](https://nbviewer.org/github/samrere/fft-from-scratch/blob/main/convolution_comparison.ipynb)
--- fft.py
+++ fft.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 DFT16=np.exp(-2j*np.pi*np.arange(16)*np.arange(16)[:,None]/16)
 FACTOR={N: np.exp(-2j*np.pi*np.arange(N/2)/ N) for N in np.array([1024//32,1024//16,1024//8,1024//4,1024//2,
                                                                   1024,
@@ -47,22 +48,3 @@ def ifft2(a,axes=(-1,-2)):
     for axis in axes:
         a=_fft(a,axis,False)
     return a
-def convolve_fft(image, kernel,mode='full'):
-    assert mode in {'full','same','valid'}, NotImplemented
-    h,w=image.shape
-    hk,wk=kernel.shape
-    # the shape of the output should be (h+hk-1, w+wk-1) for full convolution,
-    # need to extend to next power of 2
-    hf,wf=1<<(h+hk-2).bit_length(),1<<(w+wk-2).bit_length()
-    img_padded=np.pad(image,((0,hf-h),(0,wf-w)))
-    kernel_padded=np.pad(kernel,((0,hf-hk),(0,wf-wk)))
-    image_hat=fft2(img_padded)
-    kernel_hat=fft2(kernel_padded)
-    output_hat=image_hat*kernel_hat
-    result_fft=np.real(ifft2(output_hat))[:(h+hk-1), :(w+wk-1)]
-    if mode=='same':
-        return result_fft[(hk//2):(h+hk//2),(wk//2):(w+wk//2)]
-    elif mode=='valid':
-        return result_fft[(hk-1):(hk-1+h-2*(hk//2)),(wk-1):(wk-1+w-2*(wk//2))]
-    elif mode=='full':
-        return result_fft
